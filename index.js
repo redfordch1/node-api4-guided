3c974cb642e9deb64e5073bc1d3c53307eb2c80c
Chase Redford <chaseredford@Chases-MacBook-Pro.local>
2020-01-16 11:09:39 -0700

    Trying to get deployed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const server = require('./api/server.js');
+const server = require("./api/server.js");
 
-const port = 4000;
+const port = proccess.env.PORT || 4000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
